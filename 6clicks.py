a4885ae9bf8a72793dacc295513f73fe165644cf
jack-michaud <mrmichaud42@gmail.com>
2016-12-19 22:49:05 -0500

    add PickLinksIn function, recursive decisions for picking links

--- 6clicks.py
+++ 6clicks.py
@@ -1,11 +1,14 @@
 import urllib2
+from bs4 import BeautifulSoup
 
 print "Choose your destination"
 
+CLICKS = 6
+
 userInput = raw_input("Point me to a valid Wikipedia article title. '<A_Title>' I trust you.\n")
-userDest = "http://en.wikipedia.org/w/index.php?title=" + userInput + "&printable=yes"
-response = urllib2.urlopen(userDest)
-destArticle = response.read()
+USERDEST = "http://en.wikipedia.org/w/index.php?title=" + userInput + "&printable=yes"
+response = urllib2.urlopen(USERDEST)
+DESTARTICLE = response.read()
 
 print "Choose your source"
 
@@ -19,3 +22,39 @@ else:
 	userSource = "http://en.wikipedia.org/w/index.php?title=" + userInput + "&printable=yes"
 	response = urllib2.urlopen(userSource)
 	sourceArticle = response.read()
+
+'''
+Html source -> Success or Fail
+
+From html source, present links to user.
+Will either return a success or failure if correct article is not reached in
+6 clicks.
+
+'''
+def PickLinksIn(articleSource):
+	if CLICKS <= 1:
+		print "Sorry, you did not reach your article in the clicks allotted.."
+		return "Fail!"
+	CLICKS -= 1
+
+	soup = BeautifulSoup(articleSource, 'html.parser')
+	print "Which link will you follow?"
+	links = [{
+			  "name": a.string,    \
+			  "link": a.get('href')\
+			 } \
+			 for a in soup.find_all('a')]
+	counter = 1
+	for a in links:
+		print counter + " : " + a['name']
+
+	link_number = raw_input("Number?\n")
+	link_href = links[link_number-1]['link']
+
+	if USERDEST == link_href:
+		print "YOU DUNNIT!"
+		return "Success!"
+	response = urllib2.urlopen(link_href)
+	sourceArticle = response.read()
+
+	PickLinksIn(sourceArticle)
